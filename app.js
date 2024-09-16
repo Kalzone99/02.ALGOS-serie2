f492b15af3a9c67f77ab57521759a579eab37650
Kevin Selvais <kalzone99@gmail.com>
2024-09-16 13:05:44 +0200

    Exos done

--- app.js
+++ app.js
@@ -84,20 +84,71 @@ arrayOfMultiples(12, 10) //➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
 arrayOfMultiples(17, 6) //➞ [17, 34, 51, 68, 85, 102]*/
 //05.EXO
 //-------------------------------------------------------
-removeLeadingTrailing= (n) => {
+/*removeLeadingTrailing= (n) => {
     let numberToChange = parseFloat(n).toString()
     //numberToChange = numberToChange.toString()
     console.log(numberToChange);
     console.log(typeof(numberToChange));
     }
 removeLeadingTrailing("230.000") //➞ "230"
-
 removeLeadingTrailing("00402") //➞ "402"
-
 removeLeadingTrailing("03.1400") //➞ "3.14"
-
 removeLeadingTrailing("30") //➞ "30"
-
-removeLeadingTrailing("00000.000000")
+removeLeadingTrailing("00000.000000")*/
 //06.EXO
 //---------------------------------------------------------
+/*const sortIt = (numsInArray) =>{
+    let sortedArray = numsInArray.sort(function(a, b) { return a - b; })
+    console.log(sortedArray);
+}
+sortIt([4, 1, 3]) //➞ [1, 3, 4]
+
+sortIt([[4], [1], [3]]) //➞ [[1], [3], [4]]
+
+sortIt([4, [1], 3]) //➞ [[1], 3, 4]
+
+sortIt([[4], 1, [3]]) //➞ [1, [3], [4]]
+
+sortIt([[3], 4, [2], [5], 1, 6]) //➞ [1, [2], [3], 4, [5], 6]*/
+//07.EXO
+//-----------------------------------------------------------
+/*const calculator = (num1, operator, num2) => {
+    //transform the operation into a string with template literals 
+   let calculation = `${num1} ${operator} ${num2}`
+   //use the string eval() function to return values
+   console.log(eval(calculation));
+} 
+calculator(2, "+", 2) //➞ 4
+
+calculator(2, "*", 2) //➞ 4
+
+calculator(4, "/", 2) //➞ 2*/
+//08.EXO   
+//-------------------------------------------------------------
+/*const areaOfCountry = (country, area) =>{
+    let percentWorldMass =((area / 148940000) * 100).toFixed(2);
+    console.log(`${country} is ${percentWorldMass}% of the world's landmass`)
+}
+
+areaOfCountry("Russia", 17098242) //➞ "Russia is 11.48% of the total world's landmass"
+
+areaOfCountry("USA", 9372610) //=> "USA is 6.29% of the total world's landmass"
+
+areaOfCountry("Iran", 1648195) //➞ "Iran is 1.11% of the total world's landmass"
+Notes
+The total world's landmass is 148,940,000 [Km^2]
+Round the result to two decimal places.*/
+//09.EXO
+//----------------------------------------------------------------
+/*function reverseWords(text) {
+    //trim before and after, split into different words... now filter to get rid of extra spaces 
+    let str = text.trim().split(" ").filter(word => word !== "");
+    console.log(str)
+    str.reverse();
+    console.log(str.join(" "));
+}
+reverseWords(" the sky is blue") //➞ "blue is sky the"
+
+reverseWords("hello   world!  ") //➞ "world! hello"
+
+reverseWords("a good example") //➞ "example good a"*/
